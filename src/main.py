27ffad6dbd66ba1d8490ffb30e6af487c7cb11c4
Alberto Barcelos <albertoprb@gmail.com>
2024-03-17 14:15:06 +0000

    Debugger config

--- src/main.py
+++ src/main.py
@@ -2,14 +2,19 @@ from typing import Union
 
 from fastapi import FastAPI
 import redis
+import debugpy  # For debugging
 
+# Debug
+debugpy.listen(("0.0.0.0", 5678))
 
+# Create the FastAPI app
 app = FastAPI()
 redis = redis.Redis(host="redis", port=6379)
 
 # Routes
 
 
+# Root route
 @app.get("/")
 def read_root():
     return {"Container FastAPI and Docker!"}
