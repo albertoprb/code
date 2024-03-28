07e69fb9cd40ec016b63bdc22e6636d8ec877ce6
Alberto Barcelos <albertoprb@gmail.com>
2024-03-28 01:19:41 +0100

    First version of dashboard with htmx

--- dashboard/service/main.py
+++ dashboard/service/main.py
@@ -4,12 +4,14 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates  # For HTML templates
 from fastapi.staticfiles import StaticFiles  # for mounting static files
 from fastapi.responses import RedirectResponse
+import copy
 
 import os  # For file paths
 import debugpy  # For debugging
 
 # Importing scripts
-from dashboard.service.udemy_stats import courses_stats
+from dashboard.service.udemy_stats.courses_stats import Courses
+import dashboard.service.udemy_stats.courses_stats as courses_stats
 
 # For debugging
 debugpy.listen(("0.0.0.0", 5678))
@@ -26,21 +28,150 @@ templates = Jinja2Templates(directory=templates_directory)
 # Mount static assets
 app.mount("/assets", StaticFiles(directory="./dashboard/assets"), name="assets")
 
+"""Load all data
+To accelerate the request, we'll load the data beforehand.
+"""
+
+# generate scatter plot of courses for chosen category
+def load_dashboard_sections(courses, subcategories_filter=[]):
+
+    subcategories_in_use = ", ".join(subcategories_filter)
+    if len(subcategories_filter) == 0: 
+        subcategories_in_use = 'all categories'
+
+    return [
+        {
+            "title": "Courses overview",
+            "description": f"Below you have a list of top 10 courses and the scatter of all courses in the following subcategories: {subcategories_in_use}.",
+            "charts": [
+                courses_stats.plot_scatter(courses.df),
+                courses_stats.plot_box_deciles(courses.df)
+            ],
+            "tables": [
+                {
+                    "title": "Top 10 courses",
+                    "data": (
+                        courses
+                        .top10_by_revenue()[['title', 'num_subscribers', 'price', 'estimated_revenue']]
+                        .rename(columns={
+                            'title': 'Course',
+                            'num_subscribers': 'Subscribers',
+                            'price': 'Price',
+                            'estimated_revenue': 'Revenue (millions USD)'}
+                        )
+                    )
+                }
+            ]
+        },
+        {
+            "title": "Ratings",
+            "description": f"How does the ratings of a course in { subcategories_in_use } could afect the number of subscribers?",
+            "charts": [
+                courses_stats.plot_scatter_ratings(courses.df)
+            ]
+        },
+        {
+            "title": "Price",
+            "description": f"How does the pricing of a course in { subcategories_in_use } could afect the number of subscribers?",
+            "charts": [
+                courses_stats.plot_scatter_price(courses.df)
+            ],
+            "tables": [
+                {
+                    "title": "Price categories",
+                    "data": courses.price_categories()
+                }
+            ]
+        },
+        {
+            "title": "Time of Publication",
+            "description": f"How does the time of publication could affect the number of subscribers of a course in { subcategories_in_use }?",
+            "charts": [
+                courses_stats.plot_time_publication(courses.df),
+                courses_stats.plot_subscribers_by_year(courses.df)
+            ]
+        },
+        {
+            "title": "Curriculum and Course Length",
+            "description": f"How does the number of curriculum items and content length in { subcategories_in_use } could affect the number of subscribers of a course?",
+            "charts": [
+                courses_stats.plot_scatter_curriculum_items(courses.df),
+                courses_stats.plot_scatter_content_length(courses.df)
+            ]
+        },
+        {
+            "title": "Topics",
+            "description": f"What are the topics most popular in a courses in { subcategories_in_use }?",
+            "charts": [
+                courses_stats.plot_topn_labels_by_count(courses.df)
+            ]
+        },
+        {
+            "title": "Instructors",
+            "description": f"What are the most popular instructors in { subcategories_in_use }?",
+            "charts": [
+                courses_stats.plot_topn_instructors_by_subscribers(courses.df),
+                courses_stats.plot_topn_instructors_by_courses(courses.df)
+            ]
+        }
+    ]
+
+# Load courses 
+data_folder_path = os.path.join(
+    os.path.dirname(__file__), "../../data/"
+)
+
+file_path = os.path.normpath(
+    data_folder_path + "courses_numerical_categorical_data.csv"
+)
+
+# First load
+courses = Courses(csv_file_path=file_path)
+all_categories = courses.subcategories()
+
+# Get data to fill dashboard
+stats = courses.summarize()
+dashboard_sections = load_dashboard_sections(courses=courses)
 
 # Routes
 @app.get("/", response_class=HTMLResponse)
-async def dashboard(request: Request,
+async def home(request: Request,
                     hx_request: Optional[str] = Header(None)):
     context = {
         "request": request,
-        "summary_stats": {
-            "num_courses": len(courses_stats.courses)
-        },
-        "chart": courses_stats.plot
+        "summary_stats": stats,
+        "sections": dashboard_sections,
+        "subcategories": all_categories
     }
 
     return templates.TemplateResponse("dashboard.html", context)
 
+@app.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request,
+                    subcategory: str = None,
+                    hx_request: Optional[str] = Header(None)):
+    
+    courses_filtered = copy.deepcopy(courses)
+
+    if subcategory is None:
+        subcategories_filter = []
+    if subcategory == "All Subcategories":
+        subcategories_filter = []
+    else:
+        subcategories_filter = [subcategory]
+        courses_filtered.filter_by_subcategories(subcategories_filter)
+
+    # Get data to fill dashboard
+    stats = courses_filtered.summarize()
+    dashboard_sections = load_dashboard_sections(courses=courses_filtered, subcategories_filter=subcategories_filter)
+
+    context = {
+        "request": request,
+        "summary_stats": stats,
+        "sections": dashboard_sections,
+        "subcategories": all_categories
+    }
+    return templates.TemplateResponse("partials/dashboard_content.html", context)
 
 @app.get("/notebook")
 async def redirect_to_new_url():
--- dashboard/service/udemy_stats/courses_stats.py
+++ dashboard/service/udemy_stats/courses_stats.py
@@ -1,6 +1,7 @@
 # Importing analysis libraries
-import pandas as pd 
-import os  # For file paths
+import pandas as pd
+from ast import literal_eval
+
 
 # Importing charting libraries
 import plotly.express as px
@@ -8,52 +9,502 @@ import plotly.io as pio
 import plotly.graph_objects as go
 
 
-data_folder_path = os.path.join(
-    os.path.dirname(__file__), "../../../data/"
-)
+class Courses:
 
-csv_file_path = os.path.normpath(
-    data_folder_path + "courses_numerical_categorical_data.csv"
-)
+    df = None
 
+    def __init__(self, csv_file_path):
+        self.load(csv_file_path)
 
-def load_courses(from_csv):
-    print("Reading JSON input")
+    def load(self, csv_file_path):
+        print("Reading JSON input")
 
-    courses = pd.read_csv(csv_file_path)
-    courses.sort_values(
-        by='num_subscribers',
-        ascending=False,
-        inplace=True
-    )
+        self.df = pd.read_csv(csv_file_path)
+        self.df.sort_values(by="num_subscribers", ascending=False, inplace=True)
 
-    print("Finished loading data from csv")
-    return courses
+        self.add_deciles()  # Add a decile column to the dataframe
 
+        print("Finished loading data from csv")
+        return self.df
 
-def plot_scatter(courses, subcategories=None):
-    print("# Preparing scatter plot")
+    def add_deciles(self):
+        # Calculate deciles and add a new column to the dataframe
+        self.df["decile"] = pd.qcut(
+            self.df["num_subscribers"],
+            q=10,
+            labels=[
+                "Decile 1",
+                "Decile 2",
+                "Decile 3",
+                "Decile 4",
+                "Decile 5",
+                "Decile 6",
+                "Decile 7",
+                "Decile 8",
+                "Decile 9",
+                "Decile 10",
+            ],
+        )
+        return self.df
 
-    # Get subcategory filteres data
-    courses_to_plot = courses
-    if subcategories is not None:
-        print("#### Filtering subcategories" + str(subcategories))
-        courses_to_plot = courses[courses['subcategory'].isin(subcategories)]
+    def summarize(self):
+        # Summary stats
+        # Averaage rate
+        # Total number of subscribers
+        summary = {
+            "num_courses": len(self.df),
+            "avg_rate": round(self.df["rating"].mean(), 2), 
+            "total_subscribers": self.df["num_subscribers"].sum(),
+            "instructors": instructors_summary_from(self.df)["instructors"].count(),
+        }
+        return summary
+
+    def top10_by_revenue(self):
+        courses_with_revenue = self.df.copy()
+
+        # Calculating potential revenue for each course
+        # Adding 80% discount because Udemy courses are often on sale. This is the minimum possible revenue.
+        # The 0.0000001 is moving it to millions
+        courses_with_revenue["estimated_revenue"] = round(
+            courses_with_revenue["num_subscribers"]
+            * courses_with_revenue["price"]
+            * 0.2
+            * 0.0000001,
+            2,
+        )
+
+        courses_top_10_revenue = courses_with_revenue.sort_values(
+            by="estimated_revenue", ascending=False
+        ).head(10)
+
+        return courses_top_10_revenue
+
+    def filter_by_subcategories(self, subcategories):
+        self.df = self.df[self.df["subcategory"].isin(subcategories)]
+        return self.df
+   
+    def price_categories(self):
+        # Creating bins for each price category
+        courses_with_price_category = self.df.copy()
+        courses_with_price_category['price_category'] = pd.cut(
+            courses_with_price_category['price'],
+            bins=5,
+            labels=['$', '$$', '$$$', '$$$$', '$$$$$']
+        )
+
+        # Calculate the stats for each price category
+        price_category_summary = (
+            courses_with_price_category
+            .groupby('price_category', observed=True)['price']
+            .agg(['min', 'max', 'mean', 'median', 'count'])
+        )
+        return price_category_summary
     
+    def subcategories(self):
+        return self.df["subcategory"].unique()
+
+
+def plot_scatter(courses):
+
     fig = px.scatter(
-        courses_to_plot,
+        courses,
         y="num_subscribers",
         x="udemy_id",
         hover_data=courses.columns,
-        render_mode='webgl'
+        render_mode="webgl",
     )
-    fig.update_layout({
-        "paper_bgcolor": "rgba(0, 0, 0, 0)"
-    })
+
     fig_html = pio.to_html(fig, full_html=False)
     print("# Finished creating scatter plot")
-    return fig_html
+    return {"title": "Scatter Plot of Number of Subscribers", "figure": fig_html}
 
 
-courses = load_courses(from_csv=csv_file_path)
-plot = plot_scatter(courses=courses, subcategories=['Mobile Development'])
+def plot_box_deciles(courses, decile_filter=[]):
+    # Create a box plot to visualize dispersion across deciles
+    fig = px.box(
+        courses[~courses["decile"].isin(decile_filter)],
+        x="num_subscribers",
+        color="decile",
+    )
+
+    # Configure chart labels and focus view
+    fig.update_layout(
+        yaxis_title="Decile",
+        xaxis_title="Total Number of Subscribers",
+        xaxis=dict(
+            range=[0, 120000]
+        ),  # Set the initial y-axis range to focus on lower deciles
+    )
+
+    # Add an annotation to explain the chart is opened in a focused view
+    fig.add_annotation(
+        text="<sup>Opened with Zoom below 120k subscribers. Use autoscale to view all data. </sup> ",
+        xref="paper",
+        yref="paper",
+        x=1,
+        y=1.05,
+        showarrow=False,
+    )
+
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating box plot with deciles")
+    return {"title": "Dispersion of Number of Subscribers per Decile", "figure": fig_html}
+
+
+def plot_scatter_ratings(courses):
+    fig = px.scatter(
+        courses,
+        y="num_subscribers",
+        x="rating",
+        trendline="ols",
+        color="decile",
+        hover_data=courses.columns,
+        render_mode="webgl",
+    )
+
+    # Configure chart labels and focus view
+    fig.update_layout(
+        xaxis_title="Rating",
+        yaxis_title="Number of Subscribers",
+        yaxis=dict(
+            range=[0, 300000]
+        ),  # Set the initial y-axis range to focus on lower deciles
+    )
+
+    # Add an annotation to explain the chart is opened in a focused view
+    fig.add_annotation(
+        text="<sup>Opened with Zoom below 300k subscribers. Use autoscale to view all data. </sup> ",
+        xref="paper",
+        yref="paper",
+        x=1,
+        y=1.05,
+        showarrow=False,
+    )
+
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating scatter of ratings")
+    return {"title": "Number of Subscribers vs. Rating", "figure": fig_html}
+
+
+def plot_scatter_price(courses):
+    # Create scatter plot
+    fig = px.scatter(
+        courses,
+        y="num_subscribers",
+        x="price",
+        trendline="ols",
+        color="decile",
+        hover_data=courses.columns
+    )
+
+    # Configure chart labels and focus view
+    fig.update_layout(
+        xaxis_title="Price",
+        yaxis_title="Number of Subscribers",
+        yaxis=dict(range=[0, 300000])  # Set the initial y-axis range to focus on lower deciles
+    )
+
+    # Add an annotation to explain the chart is opened in a focused view
+    fig.add_annotation(
+        text="<sup>Opened with Zoom below 300k subscribers. Use autoscale to view all data. </sup> ",
+        xref="paper", yref="paper",
+        x=1, y=1.05,
+        showarrow=False,
+    )
+
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating scatter of price")
+    return {"title": "Number of Subscribers vs. Price", "figure": fig_html}
+
+
+def plot_time_publication(courses):
+    # Prepare a new dataframe with created year and sorted by year
+    courses_with_created_year = courses.copy()
+    courses_with_created_year['created_year'] = (
+        courses_with_created_year['created']
+        .apply(lambda date: pd.Timestamp(date).year)
+    )
+
+    # Sort by created date to create line plot
+    df_line_trace = courses_with_created_year.sort_values(by='created', ascending=True)
+    df_line_trace.reset_index(drop=True, inplace=True) # Reset index and drop the old index
+
+    # Plot line with course 'index' over the time
+    line_trace = go.Scatter(
+        x=df_line_trace['created'], 
+        y=df_line_trace.index,
+        name="Cumulative courses created"
+    )
+
+    # Counting the values per year
+    df_bar_trace = (
+        courses_with_created_year['created_year']
+        .value_counts()
+        .reset_index()
+    )
+
+    # Plotting the number of courses per year
+    bar_trace = go.Bar(
+        y=df_bar_trace['count'], 
+        x=df_bar_trace['created_year'],
+        name="Courses created per year"
+    )
+
+    fig = go.Figure(data=[line_trace, bar_trace])
+
+    fig.update_xaxes(tickvals=courses_with_created_year['created_year'], tickformat="%Y")
+    
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating scatter of time of publication")
+    return {"title": "Courses over time", "figure": fig_html}
+
+def plot_subscribers_by_year(courses):
+    courses_with_created_year = courses.copy()
+    courses_with_created_year['created_year'] = (
+        courses_with_created_year['created']
+        .apply(lambda date: pd.Timestamp(date).year)
+    )
+
+    #courses_with_created_year = courses_with_created_year[~courses_with_created_year['decile'].isin(['Decile 10'])] # Remove the last decile
+
+    # Create box chart
+    fig = px.box(
+        courses_with_created_year,
+        x='created_year',
+        y='num_subscribers',
+        hover_data=courses_with_created_year.columns
+    )
+
+    # Configure chart labels and focus view
+    fig.update_layout(
+        xaxis_title="Year",
+        yaxis_title="Number of subscribers",
+        yaxis=dict(range=[0, 75000])  # Set the initial y-axis range to focus on lower deciles
+    )
+
+    fig.update_xaxes(tickvals=courses_with_created_year['created_year'], tickformat="%Y")
+
+    fig.add_annotation(
+        text="<sup>Opened with Zoom below 75k subscribers. Use autoscale to view all data. </sup> ",
+        xref="paper", yref="paper",
+        x=1, y=1.05,
+        showarrow=False,
+    )
+
+    # Show the box plot
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating plot of subscribers by year")
+    return {"title": "Number of subscribers by year", "figure": fig_html}
+
+def plot_scatter_curriculum_items(courses):
+    fig = px.scatter(
+        courses,
+        x="num_curriculum_items",
+        y="num_subscribers",
+        trendline="ols",
+        color="decile",
+        hover_data=courses.columns
+    )
+
+    fig.update_layout(
+        height=600,
+        yaxis=dict(range=[0, 1000000])  # Set the initial y-axis range to make the visual less affected by outliers
+    )
+
+    # Add an annotation to explain the chart is opened in a focused view
+    fig.add_annotation(
+        text="<sup>Opened with Zoom below 1M subscribers. Use autoscale to view all data. </sup> ",
+        xref="paper", yref="paper",
+        x=1, y=1.05,
+        showarrow=False,
+    )
+
+    # Show the box plot
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating scatter plot of curriculum items")
+    return {"title": "Number of Subscribers vs Curriculum Items", "figure": fig_html}
+
+def plot_scatter_content_length(courses):
+    fig = px.scatter(
+        courses, 
+        x="content_length_hours",
+        y="num_subscribers",
+        trendline="ols",
+        color="decile",
+        hover_data=courses.columns
+    )
+
+    fig.update_layout(
+        height=600,
+        yaxis=dict(range=[0, 1000000])  # Set the initial y-axis range to make the visual less affected by outliers
+    )
+
+    # Add an annotation to explain the chart is opened in a focused view
+    fig.add_annotation(
+        text="<sup>Opened with Zoom below 1M subscribers. Use autoscale to view all data. </sup> ",
+        xref="paper", yref="paper",
+        x=1, y=1.05,
+        showarrow=False,
+    )
+
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating scatter plot of content length")
+    return {"title": "Number of Subscribers vs Content length (hours)", "figure": fig_html}
+
+def explode_labels(df):
+    # Create a working copy of the dataframe
+    courses_exploded_labels = df.copy()
+
+    # Ensure labels are treated as a list (not strings)
+    courses_exploded_labels['labels'] = courses_exploded_labels['labels'].apply(literal_eval)
+
+    # Explode the rows (one row per label in course)
+    courses_exploded_labels = courses_exploded_labels.explode('labels')
+
+    # Clean up rows with no labels ('No labels')
+    courses_exploded_labels['labels'] = courses_exploded_labels['labels'].fillna('No Label')
+
+    # Sort the rows based on label name
+    courses_exploded_labels.sort_values(by='labels', ascending=True, inplace=True)
+
+    return courses_exploded_labels
+
+def plot_topn_labels_by_count(courses, n=50):
+
+    courses = explode_labels(courses)
+
+    top50_label_count = (
+        courses
+        .groupby('labels')['num_subscribers']
+        .agg(['min', 'max', 'mean', 'median', 'sum', 'count'])
+        .reset_index()
+        .sort_values(by='count', ascending=False)
+        .head(n)
+    )
+
+    fig = px.histogram(
+        top50_label_count,
+        hover_data=top50_label_count.columns,
+        x="labels",
+        y="count",
+        labels={'labels': 'Label name', 'count': 'Courses'}
+    )
+
+    fig.update_xaxes(
+        tickvals=top50_label_count['labels'], tickfont=dict(size=10)
+    )
+
+    fig.update_layout(
+        height=600
+    )
+
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating plot of top N labels by number of courses")
+    return {"title": "Top 50 Labels by number of courses", "figure": fig_html}
+
+def plot_topn_labels_by_subscribers(courses, n=50):
+
+    courses = explode_labels(courses)
+
+    top50_label_subscribers = (
+        courses
+        .groupby('labels')['num_subscribers']
+        .agg(['min', 'max', 'mean', 'median', 'sum', 'count'])
+        .reset_index()
+        .sort_values(by='sum', ascending=False)
+        .head(n)
+    )
+
+    fig = px.histogram(
+        top50_label_subscribers,
+        hover_data=top50_label_subscribers.columns,
+        x="labels",
+        y="sum",
+        labels={'labels': 'Label name', 'sum': 'Subscribers'}
+    )
+
+    fig.update_xaxes(tickvals=top50_label_subscribers['labels'], tickfont=dict(size=10))
+
+    fig.update_layout(
+        height=600
+    )
+
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating plot of top N labels by subscribers")
+    return {"title": "Top 50 labels by Number of subscribers", "figure": fig_html}
+
+def instructors_summary_from(df):
+    # Create a working copy of the dataframe
+    courses_exploded_instructors = df.copy()
+
+    # Ensure instructors are treated as lists not strings
+    courses_exploded_instructors['instructors'] = courses_exploded_instructors['instructors'].apply(literal_eval)
+
+    # Explode the courses list with one instructor per row
+    courses_exploded_instructors = courses_exploded_instructors.explode('instructors')
+
+    # Organize instructors list for visualization
+    instructors_stats_summary = (courses_exploded_instructors.pivot_table(
+        index='instructors',  # Group by instructor
+        values=['udemy_id', 'num_subscribers'],  # Keep these 2 columns
+        aggfunc={
+            'udemy_id': 'count', # Count courses
+            'num_subscribers': 'sum'  # Sum subscribers
+        },
+    ))
+
+    # Reorganize columns for visualization
+    instructors_stats_summary.reset_index(inplace=True)
+    instructors_stats_summary.rename(columns={'udemy_id': 'num_courses'}, inplace=True)
+
+    return instructors_stats_summary
+
+def plot_topn_instructors_by_subscribers(courses, subscribers_threshold=100000):
+
+    instructors_stats_summary = instructors_summary_from(courses)
+
+    # Get instructors with more than 1M subscribers then sort by number of subscribers. 
+    instructors_1M_subscribers = (
+        instructors_stats_summary[instructors_stats_summary['num_subscribers'] > subscribers_threshold]
+        .sort_values(by='num_subscribers', ascending=False) 
+    )
+
+    # Chart results
+    fig = px.histogram(
+        instructors_1M_subscribers,
+        hover_data=instructors_1M_subscribers.columns,
+        x="instructors",
+        y="num_subscribers",
+        labels={'instructors': 'Instructor name', 'num_subscribers': 'Number of Subscribers'}
+    )
+    fig.update_xaxes(tickfont=dict(size=10))
+    fig.update_layout(
+        height=600
+    )
+
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating plot of top instructors by subscribers count")
+    return {"title": f"Instructors with more than {subscribers_threshold} subscribers", "figure": fig_html}
+
+def plot_topn_instructors_by_courses(courses, n=10):
+
+    instructors_stats_summary = instructors_summary_from(courses)
+    instructors_stats_summary = (
+        instructors_stats_summary
+        .sort_values(by='num_courses', ascending=False)
+        .head(n)
+    )
+
+    fig = px.histogram(
+        instructors_stats_summary,
+        hover_data=instructors_stats_summary.columns,	
+        x="instructors",
+        y="num_courses",
+        labels={'instructors': 'Instructor name', 'num_courses': 'Courses'}
+    )
+    fig.update_xaxes(tickfont=dict(size=10))
+    fig.update_layout(height=600)
+
+    fig_html = pio.to_html(fig, full_html=False)
+    print("# Finished creating plot of top instructors by course count")
+    return {"title": "Number of courses created by Instructors with more than 1M subscribers", "figure": fig_html}
